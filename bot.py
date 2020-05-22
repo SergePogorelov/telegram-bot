7591b12921fa0567baccd9923b683def9677aa77
Sergey <Serge.pogorelov@gmail.com>
2020-05-22 17:04:35 +0300

    add trans, modern check hw

--- bot.py
+++ bot.py
@@ -8,16 +8,19 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, InlineQueryHan
 from telegram import InlineQueryResultArticle, InputTextMessageContent
 
 
-load_dotenv()
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
+
+load_dotenv()
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 
-updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
-dispatcher = updater.dispatcher
-
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+LAYOUT = dict(zip(map(ord, "qwertyuiop[]asdfghjkl;'zxcvbnm,./`67йцукенгшщзхъфывапролджэячсмитьбю.ё67"
+                            'QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~^&ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё:?'),
+                            "йцукенгшщзхъфывапролджэячсмитьбю.ё67qwertyuiop[]asdfghjkl;'zxcvbnm,./`67"
+                            'ЙЦУКЕНГШЩЗХЪФЫВАПРОЛДЖЭЯЧСМИТЬБЮ,Ё:?QWERTYUIOP{}ASDFGHJKL:"ZXCVBNM<>?~^&'))
 
 
 def parse_homework_status(homework):
@@ -28,34 +31,59 @@ def parse_homework_status(homework):
         verdict = "Ревьюеру всё понравилось, можно приступать к следующему уроку."
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
-def get_homework_statuses(current_timestamp):
+
+def get_homework_statuses(context):
+    current_timestamp = context.job.context
     headers = {"Authorization": f"OAuth {PRACTICUM_TOKEN}"}
     params = {"from_date": current_timestamp}
-    homework_statuses = requests.get("https://praktikum.yandex.ru/api/user_api/homework_statuses/", headers=headers, params=params)
-    return homework_statuses.json()
+    homework_statuses = requests.get("https://praktikum.yandex.ru/api/user_api/homework_statuses/", headers=headers, params=params).json()
+    if homework_statuses.get("homeworks"):
+        context.bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=parse_homework_status(homework_statuses.get("homeworks")[0]))
+        context.job.context = homework_statuses.get("current_date")
 
-def check_hw():
-    current_timestamp = int(time.time()) - 60*60*24
-    new_homework = get_homework_statuses(current_timestamp)
-    if new_homework.get("homeworks"):
-        return parse_homework_status(new_homework.get("homeworks")[0])
-    return ("Работа еще не проверена!")
-    
+
+def hw(update, context):
+    if update.effective_chat.id == int(TELEGRAM_CHAT_ID):
+        current_timestamp = int(time.time()) 
+
+        if context.args:
+            try:
+                arg = int(context.args[0])
+            except (ValueError):
+                update.message.reply_text('Попробуйте "/hw <часы>"')
+                return
+
+            current_timestamp = arg*60*60
+            if current_timestamp < 0:
+                update.message.reply_text('Простите, мы не можем вернуться в прошлое! :)')
+                return
+
+        if 'job_hw_check' in context.chat_data:
+            old_job_hw_check = context.chat_data['job_hw_check']
+            old_job_hw_check.schedule_removal()
+        
+        new_job_hw_check = context.job_queue.run_repeating(get_homework_statuses, interval=300, first=0, context=current_timestamp)
+        context.chat_data['job_hw_check'] = new_job_hw_check
+        update.message.reply_text('Пошел узнавать!\nКак только твою домашнюю работу проверят, сообщу результат.')
+
+    else: 
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Эта команда не поддерживается.\nПопробуйте написать /caps")
+
+
+def hws(update, context):
+    if 'job_hw_check' not in context.chat_data:
+        update.message.reply_text('Проверка статуса домашней работы еще не запущена.\nДля запуска напиши:\n"/hw <часы>"')
+        return
+
+    job = context.chat_data['job_hw_check']
+    job.schedule_removal()
+    del context.chat_data['job_hw_check']
+
+    update.message.reply_text('Остановил проверку статуса. Для повторного запуска напиши:\n"/hw <часы>"')
+
 
 def start(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-
-dispatcher.add_handler(CommandHandler('start', start))
-
-
-def echo(update, context):
-    if update.message.text.upper() == "HW" and update.effective_chat.id == int(TELEGRAM_CHAT_ID):
-        context.bot.send_message(chat_id=update.effective_chat.id, text=check_hw())
-    else:    
-        context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
-
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
 
 
 def caps(update, context):
@@ -64,31 +92,61 @@ def caps(update, context):
         text_caps = ' '.join(context.args).upper()
     context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
 
-dispatcher.add_handler(CommandHandler('caps', caps))
+
+def trans(update, context):
+    text_trans = "Добавьте текст после /trans\nПример: '/trans ntrcn'\nЯ верну: 'текст'"
+    if context.args:
+        text_trans = ' '.join(context.args).translate(LAYOUT)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_trans)
 
 
-def inline_caps(update, context):
+def inline_trans(update, context):
     query = update.inline_query.query
     if not query:
         return
     results = list()
     results.append(
         InlineQueryResultArticle(
-            id=query.upper(),
-            title='Caps',
-            input_message_content=InputTextMessageContent(query.upper())
+            id=query,
+            title='Trans',
+            description='EN->RU',
+            input_message_content=InputTextMessageContent(query.translate(LAYOUT))
         )
     )
     context.bot.answer_inline_query(update.inline_query.id, results)
-
-dispatcher.add_handler(InlineQueryHandler(inline_caps))
 
 
 def unknow_comand(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Эта команда не поддерживается.\nПопробуйте написать /caps")
 
-dispatcher.add_handler(MessageHandler(Filters.command, unknow_comand))
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 
-updater.start_polling()
-updater.idle()
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+
+    updater = Updater(token=TELEGRAM_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("hw", hw))
+    dispatcher.add_handler(CommandHandler("hws", hws))
+    dispatcher.add_handler(CommandHandler('start', start))
+    dispatcher.add_handler(CommandHandler('caps', caps))
+    dispatcher.add_handler(CommandHandler('trans', trans))
+    dispatcher.add_handler(InlineQueryHandler(inline_trans))
+    dispatcher.add_handler(MessageHandler(Filters.command, unknow_comand))
+    dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
+    dispatcher.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
